66a4090a9db348edecd831b78b735232b933e5ac
Amir Sabagh <amir.sabagh80@gmail.com>
2022-12-18 23:01:44 -0500

    Day 8

---
+++ Day 8/main.js
@@ -0,0 +1,58 @@
+function validTime(str) {
+    //  write code here.
+    const time = str.split(":");
+    let res = false;
+    if(time[0] < 24) {
+        if(time[1] < 60) res = true;
+    }
+    return res;
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
