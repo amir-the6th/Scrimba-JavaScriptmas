a89d471dd35f130c41fde53973820b87b1906fad
Amir Sabagh <amir.sabagh80@gmail.com>
2022-12-19 10:04:53 -0500

    Day 19

---
+++ Day 19/main.js
@@ -0,0 +1,44 @@
+function centuryFromYear(num) {
+    //  write code here.
+    
+    //Uing Math.ceil() to round up
+    return Math.ceil(num/100);
+    
+    //Using Math.floor()
+    //return Math.floor((num - 1) / 100) + 1;
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('centuryFromYear()', () => {
+    it('returns current century', () => {
+        // arrange
+        const year = 1905;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(20);
+    });
+    
+    it('returns current century for edge case of start of century', () => {
+        // arrange
+        const year = 1700;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(17);
+    });    
+});
